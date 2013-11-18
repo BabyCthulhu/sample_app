70034b9416cf7cdf74eb4ebc274aae7d54bcf529
Markus A. Kollotzek <kollotzek@louis.info>
2013-11-18 10:12:26 +0100

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,16 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessible :email, :name, :password, :password_confirmation, :remember_token
   has_secure_password
 
   validates :name,  presence: true, length: { maximum: 50 }
@@ -21,4 +22,11 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   before_save { self.email.downcase! }
+  before_save :create_remember_token
+
+  private
+
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64 
+  end
 end
