0524c8a0ae6e1f85a04a6b5b4e9366d1a5a64aab
Markus A. Kollotzek <kollotzek@louis.info>
2013-11-22 14:10:01 +0100

    Add microposts model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,13 +59,6 @@ class UsersController < ApplicationController
 
   private 
 
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_path, notice: "Please sign in." unless signed_in?
-    end
-  end
-
   def signed_off_user
     redirect_to(root_path) if signed_in? 
   end
